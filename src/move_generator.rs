5ae5c5915f27465e395d6bca4e62348533f281f2
Denis Gruzdev <codingjerk@gmail.com>
2022-09-07 00:48:28 +0000

    feat: add bishop, rook and queen dummy generators

--- src/move_generator.rs
+++ src/move_generator.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 // TODO: calculate
 const MAX_MOVE_BUFFER_SIZE: usize = 100;
 
+type MoveBuffer = StaticBuffer<Move, MAX_MOVE_BUFFER_SIZE>;
+
 // TODO: move to mailbox8x8
 pub struct MoveGenerator;
 
@@ -14,7 +16,7 @@ impl MoveGenerator {
     pub fn generate(
         &self,
         board: &Board,
-        buffer: &mut StaticBuffer<Move, MAX_MOVE_BUFFER_SIZE>,
+        buffer: &mut MoveBuffer,
     ) {
         for square in Square::iter() {
             let piece = board.piece(square);
@@ -25,6 +27,9 @@ impl MoveGenerator {
             let piece_gen = match piece.dignity() {
                 p if p == Pawn => Self::generate_for_pawn,
                 p if p == Knight => Self::generate_for_knight,
+                p if p == Bishop => Self::generate_for_bishop,
+                p if p == Rook => Self::generate_for_rook,
+                p if p == Queen => Self::generate_for_queen,
                 _ => continue,
             };
 
@@ -36,7 +41,7 @@ impl MoveGenerator {
         &self,
         from: Square,
         board: &Board,
-        buffer: &mut StaticBuffer<Move, MAX_MOVE_BUFFER_SIZE>,
+        buffer: &mut MoveBuffer,
     ) {
         let c = board.side_to_move();
         buffer.add(Move::pawn_single(from, from.forward(c, 1)));
@@ -47,17 +52,17 @@ impl MoveGenerator {
         &self,
         from: Square,
         board: &Board,
-        buffer: &mut StaticBuffer<Move, MAX_MOVE_BUFFER_SIZE>,
+        buffer: &mut MoveBuffer,
     ) {
         for (dx, dy) in [
             (-2, -1),
             (-1, -2),
-            (-2, 1),
-            (-1, 2),
-            (2, -1),
-            (1, -2),
-            (2, 1),
-            (1, 2),
+            (-2,  1),
+            (-1,  2),
+            ( 2, -1),
+            ( 1, -2),
+            ( 2,  1),
+            ( 1,  2),
         ] {
             let to = match from.by(dx, dy) {
                 Some(s) => s,
@@ -69,6 +74,106 @@ impl MoveGenerator {
             }
         }
     }
+
+    fn generate_for_bishop(
+        &self,
+        from: Square,
+        board: &Board,
+        buffer: &mut MoveBuffer,
+    ) {
+        for (dx, dy) in [
+            (-1, -1),
+            (-1,  1),
+            ( 1, -1),
+            ( 1,  1),
+        ] {
+            for d in 1..8 {
+                let to = match from.by(dx * d, dy * d) {
+                    Some(s) => s,
+                    None => break,
+                };
+
+                let dest = board.piece(to);
+                if dest == PieceNone {
+                    buffer.add(Move::quiet(from, to));
+                } else {
+                    if dest.color() != board.side_to_move() {
+                        buffer.add(Move::capture(from, to, dest.dignity()));
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    fn generate_for_rook(
+        &self,
+        from: Square,
+        board: &Board,
+        buffer: &mut MoveBuffer,
+    ) {
+        for (dx, dy) in [
+            ( 0,  1),
+            ( 0, -1),
+            ( 1,  0),
+            (-1,  0),
+        ] {
+            for d in 1..8 {
+                let to = match from.by(dx * d, dy * d) {
+                    Some(s) => s,
+                    None => break,
+                };
+
+                let dest = board.piece(to);
+                if dest == PieceNone {
+                    buffer.add(Move::quiet(from, to));
+                } else {
+                    if dest.color() != board.side_to_move() {
+                        buffer.add(Move::capture(from, to, dest.dignity()));
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    fn generate_for_queen(
+        &self,
+        from: Square,
+        board: &Board,
+        buffer: &mut MoveBuffer,
+    ) {
+        for (dx, dy) in [
+            // Bishop directions
+            (-1, -1),
+            (-1,  1),
+            ( 1, -1),
+            ( 1,  1),
+
+            // Rook directions
+            ( 0,  1),
+            ( 0, -1),
+            ( 1,  0),
+            (-1,  0),
+        ] {
+            for d in 1..8 {
+                let to = match from.by(dx * d, dy * d) {
+                    Some(s) => s,
+                    None => break,
+                };
+
+                let dest = board.piece(to);
+                if dest == PieceNone {
+                    buffer.add(Move::quiet(from, to));
+                } else {
+                    if dest.color() != board.side_to_move() {
+                        buffer.add(Move::capture(from, to, dest.dignity()));
+                    }
+                    break;
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -76,10 +181,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn startpos_moves() {
+    fn startpos() {
         let board = Board::from_fen(b"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
         let movegen = MoveGenerator::new();
-        let mut buffer = StaticBuffer::new();
+        let mut buffer = MoveBuffer::new();
 
         movegen.generate(&board, &mut buffer);
 
@@ -107,5 +212,109 @@ mod tests {
         assert!(buffer.contains(Move::quiet(g1, h3)));
 
         assert_eq!(buffer.len(), 20);
+    }
+
+    #[test]
+    fn bishop() {
+        let board = Board::from_fen(b"8/8/5r2/8/3B4/8/5B2/8 w - - 0 1");
+        let movegen = MoveGenerator::new();
+        let mut buffer = MoveBuffer::new();
+
+        movegen.generate(&board, &mut buffer);
+
+        // d4 bishop
+        assert!(buffer.contains(Move::quiet(d4, c5)));
+        assert!(buffer.contains(Move::quiet(d4, b6)));
+        assert!(buffer.contains(Move::quiet(d4, a7)));
+
+        assert!(buffer.contains(Move::quiet(d4, c3)));
+        assert!(buffer.contains(Move::quiet(d4, b2)));
+        assert!(buffer.contains(Move::quiet(d4, a1)));
+
+        assert!(buffer.contains(Move::quiet(d4, e5)));
+        assert!(buffer.contains(Move::capture(d4, f6, Rook)));
+
+        assert!(buffer.contains(Move::quiet(d4, e3)));
+
+        // f2 bishop
+        assert!(buffer.contains(Move::quiet(f2, e3)));
+        assert!(buffer.contains(Move::quiet(f2, e1)));
+        assert!(buffer.contains(Move::quiet(f2, g1)));
+        assert!(buffer.contains(Move::quiet(f2, g3)));
+        assert!(buffer.contains(Move::quiet(f2, h4)));
+
+        assert_eq!(buffer.len(), 14);
+    }
+
+    #[test]
+    fn rook() {
+        let board = Board::from_fen(b"8/8/8/1n6/8/8/1R4R1/8 w - - 0 1");
+        let movegen = MoveGenerator::new();
+        let mut buffer = MoveBuffer::new();
+
+        movegen.generate(&board, &mut buffer);
+
+        // b2 rook
+        assert!(buffer.contains(Move::quiet(b2, b1)));
+        assert!(buffer.contains(Move::quiet(b2, b3)));
+        assert!(buffer.contains(Move::quiet(b2, b4)));
+        assert!(buffer.contains(Move::capture(b2, b5, Knight)));
+
+        assert!(buffer.contains(Move::quiet(b2, a2)));
+        assert!(buffer.contains(Move::quiet(b2, c2)));
+        assert!(buffer.contains(Move::quiet(b2, d2)));
+        assert!(buffer.contains(Move::quiet(b2, e2)));
+        assert!(buffer.contains(Move::quiet(b2, f2)));
+
+        // g2 rook
+        assert!(buffer.contains(Move::quiet(g2, c2)));
+        assert!(buffer.contains(Move::quiet(g2, d2)));
+        assert!(buffer.contains(Move::quiet(g2, e2)));
+        assert!(buffer.contains(Move::quiet(g2, f2)));
+        assert!(buffer.contains(Move::quiet(g2, h2)));
+
+        assert!(buffer.contains(Move::quiet(g2, g1)));
+        assert!(buffer.contains(Move::quiet(g2, g3)));
+        assert!(buffer.contains(Move::quiet(g2, g4)));
+        assert!(buffer.contains(Move::quiet(g2, g5)));
+        assert!(buffer.contains(Move::quiet(g2, g6)));
+        assert!(buffer.contains(Move::quiet(g2, g7)));
+        assert!(buffer.contains(Move::quiet(g2, g8)));
+
+        assert_eq!(buffer.len(), 21);
+    }
+
+    #[test]
+    fn queen() {
+        let board = Board::from_fen(b"8/n7/3p1b2/8/1k1Q1r2/2q1n3/3p4/8 w - - 0 1");
+        let movegen = MoveGenerator::new();
+        let mut buffer = MoveBuffer::new();
+
+        movegen.generate(&board, &mut buffer);
+
+        assert!(buffer.contains(Move::quiet(d4, d5)));
+        assert!(buffer.contains(Move::capture(d4, d6, Pawn)));
+
+        assert!(buffer.contains(Move::quiet(d4, e5)));
+        assert!(buffer.contains(Move::capture(d4, f6, Bishop)));
+
+        assert!(buffer.contains(Move::quiet(d4, e4)));
+        assert!(buffer.contains(Move::capture(d4, f4, Rook)));
+
+        assert!(buffer.contains(Move::capture(d4, e3, Knight)));
+
+        assert!(buffer.contains(Move::quiet(d4, d3)));
+        assert!(buffer.contains(Move::capture(d4, d2, Pawn)));
+
+        assert!(buffer.contains(Move::capture(d4, c3, Queen)));
+
+        assert!(buffer.contains(Move::quiet(d4, c4)));
+        assert!(buffer.contains(Move::capture(d4, b4, King)));
+
+        assert!(buffer.contains(Move::quiet(d4, c5)));
+        assert!(buffer.contains(Move::quiet(d4, b6)));
+        assert!(buffer.contains(Move::capture(d4, a7, Knight)));
+
+        assert_eq!(buffer.len(), 15);
     }
 }
